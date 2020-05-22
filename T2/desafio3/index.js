5bff6613d33f429e87056b81f101239c9a22a19a
V-Coyote <vinicius.almeida.r@gmail.com>
2020-05-22 16:15:35 -0300

    Create server with static page with challanges

---
+++ T2/desafio3/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = require('./routes');
+
+const path = require('path');
+const port = 5000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.listen(port, () => {
+  console.log(`Server was started -> port${port}`);
+});
