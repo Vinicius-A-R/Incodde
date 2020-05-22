929bba5f459384a2fa49ab8397f423f6d2bacc1c
V-Coyote <vinicius.almeida.r@gmail.com>
2020-05-22 18:20:57 -0300

    2 lines for help to access

--- T2/desafio3/index.js
+++ T2/desafio3/index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const path = require('path');
 const port = 5000;
 
+const localHost = 'http://localhost:5000';
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'src')));
 
 app.listen(port, () => {
   console.log(`Server was started -> port: ${port}`);
+  console.log(`Click here to access -> ${localHost}/crud.html`);
+  console.log(`Click here to access -> ${localHost}/google.html`);
 });
